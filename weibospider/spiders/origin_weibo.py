3397a6b126a8d189b045eb0f5506c8cc0a262b49
demonsheart <a2509875617@gmail.com>
2022-12-04 18:54:54 +0800

    完善OriginWeiboSpider

--- weibospider/spiders/origin_weibo.py
+++ weibospider/spiders/origin_weibo.py
@@ -1,64 +1,47 @@
 import scrapy
 import json
 from scrapy.http import Request
-from weibospider.items import OriginWeiboItem
+from weibospider.mytools.common import parse_bloc, parse_long_bloc
 
 
+# scrapy crawl origin_weibo -a max_page=5
 class OriginWeiboSpider(scrapy.Spider):
     name = 'origin_weibo'
     # allowed_domains = ['weibo.com']
-    base_url = "https://weibo.com/ajax/statuses/mymblog"  # 微博的接口
-    cur_page = 1
+    base_url = "https://weibo.com"  # 微博的接口
+
+    def __init__(self, max_page=None, *args, **kwargs):
+        super(OriginWeiboSpider, self).__init__(*args, **kwargs)
+        self.max_page = max_page
 
     def start_requests(self):
-        pids = ['6239620007']  # 深圳大学的pid
-        # https://weibo.com/ajax/statuses/mymblog?uid=6239620007&page=1
-        start_urls = [f'{self.base_url}?uid={pid}&page={self.cur_page}' for pid in pids]
-        for url in start_urls:
-            yield Request(url, callback=self.parse)
+        # 这里user_ids可替换成实际待采集的数据
+        user_ids = ['6239620007']  # 深圳大学的pid
+        for user_id in user_ids:
+            url = f"https://weibo.com/ajax/statuses/mymblog?uid={user_id}&page=1"
+            yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': 1})
 
-    def parse(self, response):
+    def parse(self, response, **kwargs):
+        self.logger.info('Parse function called on %s', response.url)
         data = json.loads(response.text)
         blocs = data['data']['list']
         for bloc in blocs:
-            origin_weibo_id = bloc['id']  # 源微博id
-            # 这里user也可以单独试着存一下
-            origin_user_id = bloc['user']['id']  # 源用户id
-            origin_weibo_content = bloc['text_raw']  # 源微博文本内容
-            publish_time = bloc['created_at']  # 发布时间
-            repost_count = bloc['reposts_count']  # 转发数
-            like_count = bloc['attitudes_count']  # 点赞数
-
-            item = OriginWeiboItem()
-            item['origin_weibo_id'] = origin_weibo_id
-            item['origin_user_id'] = origin_user_id
-            item['origin_weibo_content'] = origin_weibo_content
-            item['publish_time'] = publish_time
-            item['repost_count'] = repost_count
-            item['like_count'] = like_count
-            print(item)
-            print("\n\n")
-            yield item
-
-# class DouyuSpider(scrapy.Spider):
-#     name = 'douyu'
-#     allowed_domains = ['douyucdn.cn']
-#
-#     baseURL = "http://capi.douyucdn.cn/api/v1/getVerticalRoom?limit=20&offset="
-#     offset = 0
-#     start_urls = [baseURL + str(offset)]
-#
-#     def parse(self, response):
-#         data_list = json.loads(response.body)['data']
-#         if len(data_list) == 0:
-#             return
-#         for data in data_list:
-#             item = DouyuItem()
-#             item['nickname'] = data['nickname']
-#             item['imagelink'] = []
-#             item['imagelink'].append(data['vertical_src'])
-#
-#             yield item
-#
-#         self.offset += 20
-#         yield scrapy.Request(self.baseURL + str(self.offset), callback=self.parse)
+            item = parse_bloc(bloc)
+            # 长文的正文需要另外请求处理
+            if bloc["isLongText"]:
+                url = "https://weibo.com/ajax/statuses/longtext?id=" + bloc['mblogid']
+                print(item)
+                print("\n\n")
+                yield Request(url, callback=parse_long_bloc, meta={'item': item})
+            else:
+                yield item
+        # 如果还有数据 就尝试请求下一页数据
+        if blocs:
+            user_id, page_num = response.meta['user_id'], response.meta['page_num']
+            page_num += 1
+            url = f"https://weibo.com/ajax/statuses/mymblog?uid={user_id}&page={page_num}"
+            if self.max_page:  # max_page限制
+                if page_num <= int(self.max_page):
+                    yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': page_num})
+            else:  # 无限制请求
+                yield Request(url, callback=self.parse, meta={'user_id': user_id, 'page_num': page_num})
