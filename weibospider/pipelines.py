5f04671025c317eb1362bc7c2c782ae4e59b7f20
haladunking <1405611311@qq.com>
2022-12-05 22:29:59 +0800

    Update pipelines.py

--- weibospider/pipelines.py
+++ weibospider/pipelines.py
@@ -35,6 +35,11 @@ class WeibospiderPipeline:
             #         ',repost_count varchar(1000)' \
             #         ',like_count varchar(1000)' \
             #         ');'
+            sql2='select * from weibo where origin_weibo_id = %s'
+            data2=item['origin_user_id']
+            sql3='update weibo set  origin_user_id=%s, origin_weibo_content=%s,publish_time=%s,repost_count=%s,like_count =%s where origin_user_id = %s'
+            data3 = ( item['origin_user_id'], item['origin_weibo_content'], item['publish_time'],
+                    item['repost_count'], item['like_count'],item['origin_weibo_id'])
             sql = 'INSERT INTO weibo(origin_weibo_id, origin_user_id, origin_weibo_content,publish_time,repost_count,like_count)VALUES(%s,%s,%s,%s,%s,%s) '
             data = (item['origin_weibo_id'], item['origin_user_id'], item['origin_weibo_content'], item['publish_time'],
                     item['repost_count'], item['like_count'])
@@ -47,6 +52,11 @@ class WeibospiderPipeline:
             #         ',repost_weibo_content varchar(1000)' \
             #         ',repost_publish_time varchar(1000)' \
             #         ');'
+            sql2='select * from repost_weibo where repost_weibo_id = %s'
+            data2=item['repost_weibo_id']
+            sql3='update repost_weibo set origin_weibo_id = %s,origin_user_id=%s, repost_user_id=%s,repost_weibo_content=%s,repost_publish_time=%s where repost_weibo_id = %s'
+            data3 = (item['origin_weibo_id'], item['origin_user_id'],  item['repost_user_id'],
+                    item['repost_weibo_content'], item['repost_publish_time'],item['repost_weibo_id'])
             sql = 'INSERT INTO repost_weibo(origin_weibo_id, origin_user_id, repost_weibo_id,repost_user_id,repost_weibo_content,repost_publish_time)VALUES(%s,%s,%s,%s,%s,%s) '
             data = (item['origin_weibo_id'], item['origin_user_id'], item['repost_weibo_id'], item['repost_user_id'],
                     item['repost_weibo_content'], item['repost_publish_time'])
@@ -57,15 +67,24 @@ class WeibospiderPipeline:
             #         ',followers_count varchar(1000)' \
             #         ',statuses_count varchar(1000)' \
             #         ');'
+            sql2='select * from user_info where user_id = %s'
+            data2=item['user_id']
+            sql3='update user_info set friends_count=%s, followers_count=%s,statuses_count =%s where user_id = %s'
+            data3 = ( item['friends_count'], item['followers_count'], item['statuses_count'],item['user_id'])
             sql = 'INSERT INTO user_info(user_id, friends_count, followers_count,statuses_count)VALUES(%s,%s,%s,%s) '
             data = (item['user_id'], item['friends_count'], item['followers_count'], item['statuses_count'])
         try:
             self.cursor.execute(database)
             # self.cursor.execute(table)
-            self.cursor.execute(sql, data)
-            self.connect.commit()
+            self.cursor.execute(sql2, data2)
+            ret = self.cursor.fetchone()
+            if ret:
+                self.cursor.execute(sql3, data3)
+            else:
+                self.cursor.execute(sql, data)
+                self.connect.commit()
         except Exception as e:
-            print('===============插入数据失败===============', e)
+            print('===============失败===============', e)
             self.connect.rollback()
         return item
 
