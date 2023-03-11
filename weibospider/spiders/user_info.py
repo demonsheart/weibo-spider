ecf08ca602b66583fe69285a64fb915fcaf6eec4
demonsheart <a2509875617@gmail.com>
2023-03-11 20:57:09 +0800

    os定时脚本(开摆)

--- weibospider/spiders/user_info.py
+++ weibospider/spiders/user_info.py
@@ -1,6 +1,7 @@
 import scrapy
 import json
 from scrapy.http import Request
+from twisted.internet import defer, reactor
 from weibospider.items import UserInfoItem
 import time
 
@@ -10,30 +11,22 @@ class UserInfoSpider(scrapy.Spider):
     # allowed_domains = ['weibo.com']
 
     base_url = "https://weibo.com/ajax/profile/info"  # 微博的接口
-    # https://weibo.com/ajax/profile/info?uid=6239439310
 
-    # 轮次
-    rounds = 5
+    # https://weibo.com/ajax/profile/info?uid=6239439310
 
     def __init__(self, *args, **kwargs):
         super(UserInfoSpider, self).__init__(*args, **kwargs)
 
     def start_requests(self):
-        uids = ['6239620007'] * 50  # 生成50条
+        uids = ['6239620007'] * 50
         start_urls = [f'{self.base_url}?uid={uid}' for uid in uids]
-        cur_round = 0
-        while self.rounds > cur_round:
-            cur_round += 1
-            print(f"===========第{cur_round}轮============")
-            for url in start_urls:
-                # 使用本机代理
-                # cur_ip = f'http://127.0.0.1:7890'
-                # yield Request(url, callback=self.parse, headers=self.headers, dont_filter=True, meta={'proxy': cur_ip})
+        for url in start_urls:
+            # 使用本机代理
+            # cur_ip = f'http://127.0.0.1:7890'
+            # yield Request(url, callback=self.parse, headers=self.headers, dont_filter=True, meta={'proxy': cur_ip})
 
-                # 默认走通过全局代理
-                yield Request(url, callback=self.parse, dont_filter=True)
-            # 每一分钟提交一次
-            time.sleep(10)
+            # 默认走通过全局代理
+            yield Request(url, callback=self.parse, dont_filter=True)
 
     def parse(self, response, **kwargs):
         data = json.loads(response.text)
